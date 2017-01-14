8af8a8e62ab78df4ab6d84dcac720b1984f36715
김태균 <ktk@gimtaegyun-ui-MacBook-Pro.local>
2017-01-14 15:25:23 +0900

    test

--- study/170108/homework.js
+++ study/170108/homework.js
@@ -23,6 +23,8 @@ function sumIf(arr,target){
     return result;
 }
 
+
+
 //4 번문제
 var inventory = [
     {name: 'apples', quantity: 2},
@@ -31,56 +33,56 @@ var inventory = [
     {name: 'grapes', quantity: 10},
     {name: 'strawberries', quantity: 8}
 ];
-
+function findIndex(arr,callback){
+    for(var i = 0; i > arr.length; i++){
+        if(arr[i].name == callback) return 1;
+    }
+}
 describe('콜백함수를 익히자', function() {
     describe('findIndex', function() {
         it('원하는 데이터의 index를 찾는다', function(){
-            expect(
-                findIndex(inventory, function(fruit) {
-                    return fruit.name === 'bananas';
-                })
-            ).toBe(1);
-            expect(
-                findIndex(inventory, function(fruit) {
-                    return fruit.name === 'cherries';
-                })
-            ).toBe(2);
+            expect( findIndex( inventory, function(fruit){return fruit.name === 'bananas';}) ).toBe(1);
+            expect( findIndex( inventory, function(fruit){return fruit.name === 'cherries';}) ).toBe(2);
         });
     });
 
     describe('template', function() {
         it('반복되는 데이터를 마크업으로 변환한다', function() {
-            expect(
-                template(inventory, function(fruit) {
-                    return '<li>'+fruit.name+'<span>수량 : '+fruit.quantity +'</span></li>';
-                })
-            ).toBe('<li>apples<span>수량 : 2</span></li><li>bananas<span>수량 : 0</span></li><li>cherries<span>수량 : 5</span></li><li>grapes<span>수량 : 10</span></li><li>strawberries<span>수량 : 8</span></li>')
+            expect( template( inventory, function(fruit){return '<li>'+fruit.name+'<span>수량 : '+fruit.quantity +'</span></li>';}) ).toBe('<li>apples<span>수량 : 2</span></li><li>bananas<span>수량 : 0</span></li><li>cherries<span>수량 : 5</span></li><li>grapes<span>수량 : 10</span></li><li>strawberries<span>수량 : 8</span></li>')
         });
     });
 });
 
 
+function isZero(num){
+    return num == 0;
+}
 
+function isNum(n){
+    return typeof n == 'number';
+}
 
 describe('170108 과제', function() {
     it('1. 배열의 숫자를 모두 더하는 sum 함수를 만드시오 - 수호님', function() {
-        expect( sum([1,2,3,4,5,6,7,8,9,10]) ).toBe(55)
-        expect( sum([10,20,77,3,12,8]) ).toBe(130)
+        expect( sum( [1,2,3,4,5,6,7,8,9,10]) ).toBe(55)
+        expect( sum( [10,20,77,3,12,8]) ).toBe(130)
     });
     it('2. 배열에서 가장 큰 숫자를 찾는 max 함수를 만드시오 - 수호님', function() {
-        expect( max([1,2,4,10,3,7]) ).toBe(10)
-        expect( max([2,10,-5,19,-4,20,12]) ).toBe(20)
+        expect( max( [1,2,4,10,3,7]) ).toBe(10)
+        expect( max( [2,10,-5,19,-4,20,12]) ).toBe(20)
     });
     it('3. 특정 숫자 이상의 숫자만 합산하는 sumif 함수를 만드시오 - 수호님', function() {
-        expect( sumIf([4,5,2,1,5,6,4,7], 6) ).toBe(13)
-        expect( sumIf([3,-4,1,9,10,20], 10) ).toBe(30)
+        expect( sumIf( [4,5,2,1,5,6,4,7], 6) ).toBe(13)
+        expect( sumIf( [3,-4,1,9,10,20], 10) ).toBe(30)
     });
     it('5. isZero 함수를 만드세요 - 욱님', function() {
-        expect( isZero(0).toBe(true) )
-        expect( isZero(!0).toBe(false) ) //!0는 0이외의 수
+        expect( isZero(0)).toBe(true)
+        expect( isZero(!0)).toBe(false)//!0는 0이외의 수
     });
     it('6. isNum 함수를 만드세요 - 욱님', function() {
-        expect( isNum(0).toBe(true) )
-        expect( isNum('0').toBe(false) )
+        expect( isNum(0)).toBe(true)
+        expect( isNum('0')).toBe(false)
     });
 });
+Contact GitHub API Training Shop Blog About
+© 2017 GitHub, Inc. Terms Privacy Security Status Help
