99deeac8eb81bce40407d835a4ce30facbb615bb
김태균 <ktk@gimtaegyun-ui-MacBook-Pro.local>
2017-01-15 23:12:34 +0900

    170115 v1

--- study/170108/homework.js
+++ study/170108/homework.js
@@ -1,28 +1,70 @@
 function sum(arr){
     var result = 0;
-    for(var i = 0;i < arr.length; i++){
+    for (var i = 0; i < arr.length; i++) {
         result += arr[i];
     }
     return result;
 }
 
-
-function max(arr) {
-    var result = arr[0];
+function max(arr){
+    var result = 0;
     for (var i = 0; i < arr.length; i++) {
-        result < arr[i] ? result=arr[i] : result;
+        if(arr[i] >= result) result = arr[i];
     }
     return result;
 }
 
 function sumIf(arr,target){
     var result = 0;
-    for(var i = 0;i < arr.length;i++){
-        if(arr[i]>=target) result+=arr[i];
+    for (var i = 0; i < arr.length; i++) {
+        if(arr[i] >= target) result += arr[i];
     }
     return result;
 }
 
+function findIndex(target , callback){
+    for (var i = 0; i < target.length; i++) {
+        if(callback(target[i])) return i
+    }
+}
+
+function template(target, callback){
+    var result='';
+    for (var i = 0; i < target.length; i++) {
+        result += callback(target[i]);
+    }
+    return result;
+}
+
+function isZero(num){
+    return num == 0;
+}
+
+function isNum(value){
+    return typeof value == 'number'
+}
+
+function getZeroOrOne(){
+    var random = Math.random();
+    return random >= 0.5 ? 1 : 0
+}
+//0 or 1을 반환
+function getRandom(num){
+    var result = 0;
+    // for (var i = 0; i < num-1; i++) {
+    //     result += getZeroOrOne();
+    // }
+    while(--num) result += getZeroOrOne();
+    return result;
+}
+
+function rangeAge(arr,target){
+    var result = target/10;
+    //정수를 만들어라
+    //4.7 = 4
+    if(target > 60) return arr[6];
+    return arr[result]
+}
 
 
 //4 번문제
@@ -33,16 +75,13 @@ var inventory = [
     {name: 'grapes', quantity: 10},
     {name: 'strawberries', quantity: 8}
 ];
-function findIndex(arr,callback){
-    for(var i = 0; i > arr.length; i++){
-        if(arr[i].name == callback) return 1;
-    }
-}
+//인벤토리의 네이임이 바나나 면 1 리턴해라
+//인벤토리의 네이임이 체리 면 1 리턴해라
 describe('콜백함수를 익히자', function() {
     describe('findIndex', function() {
         it('원하는 데이터의 index를 찾는다', function(){
             expect( findIndex( inventory, function(fruit){return fruit.name === 'bananas';}) ).toBe(1);
-            expect( findIndex( inventory, function(fruit){return fruit.name === 'cherries';}) ).toBe(2);
+            //expect( findIndex( inventory, function(fruit){return fruit.name === 'cherries';}) ).toBe(2);
         });
     });
 
@@ -52,16 +91,6 @@ describe('콜백함수를 익히자', function() {
         });
     });
 });
-
-
-function isZero(num){
-    return num == 0;
-}
-
-function isNum(n){
-    return typeof n == 'number';
-}
-
 describe('170108 과제', function() {
     it('1. 배열의 숫자를 모두 더하는 sum 함수를 만드시오 - 수호님', function() {
         expect( sum( [1,2,3,4,5,6,7,8,9,10]) ).toBe(55)
@@ -83,6 +112,11 @@ describe('170108 과제', function() {
         expect( isNum(0)).toBe(true)
         expect( isNum('0')).toBe(false)
     });
+    it('7. getRandom함수를 만드세요 - 욱님', function() {
+        expect( getRandom(5)).toMatch(/[0-4]/)
+    });
+    it('8. rangeAge함수를 만드세요 - 욱님', function() {
+        expect( rangeAge(['아동', '십대', '이십대', '삼십대', '사십대', '오십대', '육십대이상'], 60)).toBe('육십대이상')
+        expect( rangeAge(['아동', '십대', '이십대', '삼십대', '사십대', '오십대', '육십대이상'], 0)).toBe('아동')
+    });
 });
-Contact GitHub API Training Shop Blog About
-© 2017 GitHub, Inc. Terms Privacy Security Status Help
