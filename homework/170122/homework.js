b8e51ed74599680c43f6bf5969cab8f1c354ef40
tuhbm <tuhbm@naver.com>
2017-01-25 17:57:36 +0900

    170125-v2

--- homework/170122/homework.js
+++ homework/170122/homework.js
@@ -32,37 +32,47 @@ var data = {
     "stringNumbers":["50", "20", "123", "12", "33", "46", "70", "21", "56", "560"]
 };
 
+//콜백함수의 인자는 순차적으로 배열의 값이 들어오고 배열의 길이만큼 실행된다.
+//filter는 return값이 true인것만 묶은 새로운 배열을 만들어준다
+//map은 콜백함수의 리턴값을 새배열로 만들어 리턴한. 각각의 인자값에 추가
+//sort는 리턴값이 참이면 첫번쨰 인자가 새로운 배열에 들어가고 거짓이면 두번쨰 인자가 배열에 들어간다.
+
 var fruits = data.fruits;
-var priceObj = fruits.filter(function(fruits){
-    var result = fruits['price'] >= 4000;
-    console.log(result);
-
-    // for (var i = 0; i < result.length; i++) {
-    //     result += result[i];
-    // }
-    return result;
+//1번 0
+var dataPrice = fruits.filter(function(fruit){
+    return fruit.price >= 4000;
 });
+console.log(dataPrice);
 
-// function searchPrice(){
-//
-//     var fruits = fruitsData[0];
-//     console.log(fruits);
-//     var result = [];
-//     // target.filter(function(){
-//     //     if(target.price >= 4000) result += target.arguments
-//     //     return result;
-//     // });
-//     for (var i = 0; i < fruits.length; i++) {
-//         console.log(fruits[i]);
-//         console.log(fruits[i].price);
-//         if(fruits[i].price >= 4000) result += fruits[i];
-//     }
-//     console.log(result);
-//
-// }
-// searchPrice();
+//2번 0
+var priceName = dataPrice.map(function(a){
+    return a.name;
+})
 
+console.log(priceName);
 
-// var above50 = numbers.filter(function(number){
-//     return number >= 50;
-// });
+//3번 0
+var priceAlign = fruits.sort(function(a,b){
+    return a.price - b.price
+});
+console.log(priceAlign);
+
+//4번 0
+var addUnit =fruits.map(function(unit){
+    unit = "name:'"+unit.name+"', quantity:'"+unit.quantity+"개'"+", price:'"+unit.price+"원'";
+    return unit;
+});
+console.log(addUnit);
+
+//5번
+var addProperty = fruits.map(function(add){
+    //add = "<tr><td>"+add.name+"</td><td>"+add.quantity+"</td><td>"+add.price+"</td></tr>"
+    //var result = add;
+    //result.join('<table>',add,'</table>');
+    var result = JSON.stringify(add)
+    result = "<tr><td>"+add.name+"</td><td>"+add.quantity+"</td><td>"+add.price+"</td></tr>"
+    result.join
+    return result;
+})
+
+console.log(addProperty);
